6263ba85e8f40d6c457ed7ad011d011fc3bd3983
mossadhagos <mossad.hagos@outlook.com>
2026-06-03 12:20:09 +0000

    feat: bronze streaming ingestion via Lakeflow pipeline

--- marathos_Mossad_Hagos/transfromation/bronze/pipline_bronz.py
+++
@@ -1,7 +0,0 @@
-import pyspark as ps 
-
-DATA_PATH = "/Volumes/marathos/default/marathos_raw/TWO_CENTURIES_OF_UM_RACES.csv"
-
-df_marathon = spark.read.csv(f"{DATA_PATH}", header=True)
-
-print(df_marathon)
